4a1beb6b2071dd1ed38ca4277318a5ac7e22339f
Austin <austin.griffith@madwiremedia.com>
2019-09-11 12:08:56 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  KirbyEthereum,
+  KirbyEthereumProvider,
+  KirbyEthereumContext,
+  useKirbySelector,
+} from "@kirby-web3/ethereum-react";
+
+import Button from "react-bootstrap/Button";
+import { getChain } from "evm-chains";
+
+function App() {
+  return (
+    <div>
+      Signator.io Initial Commit
+    </div>
+  );
+}
+
+export default App;
