474ce991f9cd31211bdef44a5df89b39e14978ee
Austin <austin.griffith@madwiremedia.com>
2019-09-20 11:25:02 -0600

    center qr on iphone screen

--- src/components/Signatures.js
+++ src/components/Signatures.js
@@ -49,7 +49,7 @@ const Signatures = props => {
     }
 
     return (
-      <div style={{ margin: 8, padding: 8, paddingBottom:16, border: "1px solid #666666", backgroundColor:"rgba(32, 32, 32, 0.8)", cursor:cursor, marginTop: "4vw"}} onClick={() => {
+      <div style={{ margin: 'auto', margin: 8, padding: 8, paddingBottom:16, border: "1px solid #666666", backgroundColor:"rgba(32, 32, 32, 0.8)", cursor:cursor, marginTop: "4vw"}} onClick={() => {
         if(!props.static){
           window.open(link);
         }
@@ -62,7 +62,7 @@ const Signatures = props => {
           🔏 {signatureObject.message}
           </Col>
         </Row>
-        <Row style={{ marginTop: 8 }}>
+        <Row style={{ margin: 'auto', marginTop: 8 }}>
           <Col>
             <Blockies
               seed={signatureObject.recover.toLowerCase()}
@@ -74,7 +74,7 @@ const Signatures = props => {
             {signatureLines}
           </Col>
           <Col>
-            <div style={{ backgroundColor: "#FFFFFF", width:blockSize,height:blockSize }}>
+            <div style={{ margin: 'auto', backgroundColor: "#FFFFFF", width:blockSize,height:blockSize }}>
               <QRCode
                 value={link}
                 size={blockSize}
